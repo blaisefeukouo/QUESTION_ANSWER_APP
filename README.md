e21c654ac78f3f8c45504f678db3dcc2f9d0d30e
Blaise Feukouo Tofing <blaise_feukjouo@yahoo.fr>
2025-11-25 01:45:58 +0100

    New commit

--- README.md
+++ README.md
@@ -2,10 +2,15 @@ For this project, following librries need to be installed
 pip install langchain pymongo bs4 openai tiktoken gradio requests lxml argparse unstructured
 pip install langchain pymongo bs4 openai tiktoken gradio requests lxml argparse unstructured langchain-openai langchain-mongodb langchain-community
 
+OR
+pip install -r requirements.txt
 
 # How to create python virtual environment
 python3 -m venv .venv
 # How to activate
-source .venv/Scripts/activate
+source .venv/Scripts/activate or source .venv/bin/activate
 # how to deactivate:
-siply type: deactivate
+simply type: deactivate
+
+# 1- Run all embedding and load them into collection_of_text_blobs
+python3 load_data.py
--- load_data.py
+++ load_data.py
@@ -1,8 +1,8 @@
 from pymongo import MongoClient
-from langchain_openai import OpenAIEmbeddings, OpenAI
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_mongodb import MongoDBAtlasVectorSearch
 from langchain_community.document_loaders import DirectoryLoader
-#from langchain.chains import RetrievalQA
+from langchain_core.prompts import ChatPromptTemplate
 import gradio as gr
 from gradio.themes.base import Base
 import key_param
@@ -12,17 +12,9 @@ dbName = "longchain_demo"
 collectionName = "collection_of_text_blobs"
 collection = client[dbName][collectionName]
 
-loader = DirectoryLoader(
-    "./sample_files",
-    glob="./*.txt",
-    show_progress=True
-)
+loader = DirectoryLoader("./sample_files", glob="./*.txt", show_progress=True)
 data = loader.load()
 embeddings = OpenAIEmbeddings(openai_api_key=key_param.open_api_key)
 #Initialize the vector store. 
 #Vectorize the text from the documents using the specified Embeddings model and insert them into the specified mongodb collection
-vector_store = MongoDBAtlasVectorSearch.from_documents(
-    data,
-    embeddings,
-    collection=collection
-)
+vector_store = MongoDBAtlasVectorSearch.from_documents(data, embeddings, collection=collection, index_name="vector_index")
